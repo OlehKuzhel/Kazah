caacf02240202e6ee5587b385f7b1fdaa1329df4
Oleh Kuzhel <olehkuzhel@gmail.com>
2020-07-05 01:40:22 +0300

    Правки

--- js/main.js
+++ js/main.js
@@ -1,13 +1,20 @@
+WebFont.load({
+    google: {
+        families: ['Montserrat:400,500,600,700:cyrillic']
+    }
+});
+
 $(document).ready(function($) {
 
-
-    // $(document).mouseup(function (e){ // событие клика по веб-документу
-    //     var div = $(".main-elements__dropdown, .link--toggle"); // тут указываем ID элемента
-    //     if (!div.is(e.target) // если клик был не по нашему блоку
-    //         && div.has(e.target).length === 0) { // и не по его дочерним элементам
-    //         div.removeClass('open'); // скрываем его
-    //     }
-    // });
+ $('.lazy').Lazy({
+        // your configuration goes here
+        scrollDirection: 'vertical',
+        effect: 'fadeIn',
+        visibleOnly: true,
+        onError: function(element) {
+            console.log('error loading ' + element.data('src'));
+        }
+    });
 
 var isMobile = false;
     if (/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|ipad|iris|kindle|Android|Silk|lge |maemo|midp|mmp|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i.test(navigator.userAgent) || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(navigator.userAgent.substr(0, 4))) isMobile = true;
@@ -137,6 +144,7 @@ var sliderTabs = new Swiper('.works-tabs-slider', {
     });
 var sliderOffice = new Swiper('.office-slider', {
         speed: 800,
+        init: false,
         slidesPerView: 'auto',
         loop: true,
         loopedSlides: 3,
@@ -173,6 +181,7 @@ var sliderSklad = new Swiper('.sklad-slider', {
         loop: true,
         loopedSlides: 3,
         spaceBetween: 20,
+        init: false,
         navigation: {
             nextEl: '.works--next',
             prevEl: '.works--prev',
@@ -198,6 +207,7 @@ var sliderSklad = new Swiper('.sklad-slider', {
         slidesPerView: 'auto',
         simulateTouch: false,
         loop: true,
+        init: false,
         loopedSlides: 3,
         spaceBetween: 20,
         navigation: {
@@ -254,13 +264,6 @@ var sliderSklad = new Swiper('.sklad-slider', {
                 slidesOffsetBefore: 0,
                 slidesOffsetAfter: 0,
             },
-            968: {
-                // simulateTouch: true,
-                // slidesPerView: 1,
-                // spaceBetween: 0,
-                // slidesOffsetBefore: marginLeftWrapper + paddingLeftWrapper,
-                // slidesOffsetAfter: marginLeftWrapper + paddingLeftWrapper,
-            },
         },
     });
 
@@ -270,12 +273,9 @@ var sliderSklad = new Swiper('.sklad-slider', {
       slidesPerView: 3,
       speed: 800,
       loop: true,
-      // loop: true,
-      // freeMode: true,
       simulateTouch: false,
-      // centerInsufficientSlides: true,
       direction: 'vertical',
-      // loopedSlides: 5, //looped slides should be the same
+      init: false,
       watchSlidesVisibility: true,
       watchSlidesProgress: true,
       breakpoints: {
@@ -302,12 +302,11 @@ var sliderSklad = new Swiper('.sklad-slider', {
       slidesPerView: 1,
       loop: true,
        simulateTouch: false,
+       init: false,
         effect: 'fade',
 	  fadeEffect: {
 			crossFade: true
 	  },
-      // loop:true,
-      // loopedSlides: 4, //looped slides should be the same
       navigation: {
         nextEl: '.big--next',
         prevEl: '.big--prev',
@@ -420,6 +419,12 @@ $('body').on('click', '.fancybtn', function(event) {
 
 setTimeout(function(){
 $('.preloader').fadeOut('400');
+galleryThumbs.init();
+galleryTop.init();
+
+sliderOffice.init();
+sliderSklad.init();
+sliderComp.init();
 $('.main-content h1').addClass('anim')
 }, 500)
  
@@ -430,32 +435,15 @@ $('.main-content h1').addClass('anim')
     container: window
 });
 
-// Cache selectors
 var lastId,
   topMenu = $(".header-menu"),
-  // topMenuHeight = topMenu.outerHeight() + 15,
-  // All list items
   menuItems = topMenu.find("a"),
-  // Anchors corresponding to menu items
   scrollItems = menuItems.map(function() {
     var item = $($(this).attr("data-href"));
     if (item.length) {
       return item;
     }
   });
-
-  // console.log(scrollItems)
-
-// Bind click handler to menu items
-// so we can get a fancy scroll animation
-// menuItems.click(function(e) {
-//   var href = $(this).attr("href"),
-//     offsetTop = href === "#" ? 0 : $(href).offset().top - topMenuHeight + 1;
-//   $('html, body').stop().animate({
-//     scrollTop: offsetTop
-//   }, 300);
-//   e.preventDefault();
-// });
 
 // Bind to scroll
 $(window).scroll(function() {
@@ -474,9 +462,6 @@ $(window).scroll(function() {
 
   if (lastId !== id) {
     lastId = id;
-    // Set/remove active class
-    console.log(id)
-    // menuItems
       $('.link--menu').removeClass("active")
      $('[data-href="#' + id +'"]').addClass("active");
   }
@@ -512,14 +497,6 @@ $(document).scroll(function(){
 	})
 
 toggleHeader();
-
-// os.on('enter', '.section-main', (element, event) => {
-//    	$('.section-header').removeClass('fixed')
-// });
-
-// os.on('leave', '.section-main', (element, event) => {
-//    	$('.section-header').addClass('fixed')
-// });
 
 
 
@@ -568,19 +545,5 @@ $('.field-phone').mask('+0 (000) 000-00-00')
     	/* Act on the event */
     });
 
-// os.on('enter', '.advanced', (element, event) => {
-//     $('.advanced').addClass('animate')
-// });
 
 });
-
-//  function windowSize(){
-//         if ($(window).width() <= '992'){
-//             $('body').find('.destinations-grid__row1').prependTo('.destinations-grid')
-//         } else {
-//             $('body').find('.destinations-grid__row1').prependTo('.destinations-grid__col2')
-//         }
-//     }
-    
-    // $(window).on('load resize',windowSize);
-
